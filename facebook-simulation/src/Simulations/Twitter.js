7463154c01252344309218d91608525a89038015
katiaStepovoy <katias3010@gmail.com>
2022-12-22 12:40:59 +0200

    server up

---
+++ facebook-simulation/src/Simulations/Twitter.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import Feed from "../components/Feed.js";
+import Contacts from "../components/Contacts.js";
+import Grid from "@mui/material/Grid";
+import TwitterMenu from "../components/TwitterMenu.js";
+
+function Twitter() {
+  return (
+    <Grid container spacing={5} style={{ backgroundColor: "black" }}>
+      <Grid item xs={2.5}>
+        <TwitterMenu />
+      </Grid>
+      <Grid item xs={7}>
+        <Feed backgroundColor={"black"} />
+      </Grid>
+      <Grid item xs={2.5}>
+        <Contacts fontColor={"white"} />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Twitter;
