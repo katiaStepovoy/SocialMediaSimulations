bb0e5409ded309e01b0724a85cd39b0e1c7090ce
katiaStepovoy <katias3010@gmail.com>
2023-03-15 13:07:35 +0200

    connect to render server

--- facebook-simulation/server/app.js
+++ facebook-simulation/server/app.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const simulationRoutes = require('./routes/routes');
+const simulationRoutes = require("./routes/routes");
 
-const mongoConnect = require('./util/database').mongoConnect;
+const mongoConnect = require("./util/database").mongoConnect;
 
 app.use(simulationRoutes);
 
 mongoConnect(() => {
-    app.listen(8080);
+  app.listen(8080);
 });
