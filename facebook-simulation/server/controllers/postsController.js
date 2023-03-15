ba9e7d92d3e83d727d87e84b257efb3ca5fc15e6
katiaStepovoy <katias3010@gmail.com>
2023-03-15 21:27:24 +0200

    post fixed

--- facebook-simulation/server/controllers/postsController.js
+++ facebook-simulation/server/controllers/postsController.js
@@ -1,20 +1,24 @@
+const bodyParser = require("body-parser");
+
 const Post = require("../models/post");
 
 exports.getPosts = (req, res) => {
   Post.fetchAll()
-  .then(res.setHeader("Access-Control-Allow-Origin", "*"))
-  .then(posts => res.send(posts))
-    .catch(err => conosole.log(err));
+    .then(res.setHeader("Access-Control-Allow-Origin", "*"))
+    .then((posts) => res.send(posts))
+    .catch((err) => conosole.log(err));
 };
 
 exports.postPost = (req, res, next) => {
   const post = new Post(
-    "5.29.9.190",
-    "have a bomb day yall",
-    "2022-10-23T18:25:43.511Z",
-    "Jayvion Bryant"
+    req.body.long,
+    req.body.lat,
+    req.body.content,
+    req.body.time,
+    req.body.username
   );
-  post.save()
-  .then(result =>  res.redirect('/facebookpost'))
-  .catch(err => console.log(arr));
+  post
+    .save()
+    .then((res) => res.redirect("/facebook"))
+    .catch((err) => console.log(err));
 };
