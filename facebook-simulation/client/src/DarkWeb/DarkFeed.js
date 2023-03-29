50b5b3211f8e41b44354b71f5c8e56dfde9cf68e
katiaStepovoy <katias3010@gmail.com>
2023-03-29 16:57:54 +0300

    dark web mockup

---
+++ facebook-simulation/client/src/DarkWeb/DarkFeed.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { Paper, Stack } from "@mui/material";
+import DarkPost from "./DarkPost.js";
+import { useState, useEffect } from "react";
+
+function DarkFeed(props) {
+  const db = props.db;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(db)
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.log(err.message));
+  }, [posts, db]);
+
+  posts.sort((post1, post2) => post1.time.localeCompare(post2.time)).reverse();
+
+  return (
+    <Paper elevation={0} sx={{ bgcolor: "transparent" }}>
+      <Stack spacing={2}>
+        {posts.map((post, index) => (
+          <DarkPost
+            key={index}
+            content={post.content}
+            date={post.time}
+            name={post.username}
+            lat={post.lat}
+            long={post.long}
+          />
+        ))}
+      </Stack>
+    </Paper>
+  );
+}
+
+export default DarkFeed;
